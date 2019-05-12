f27c75847582f8d088d4662c672cb072a322c217
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2019-05-12 11:14:43 +0200

    move config to separate file

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#define VOLTAGE_SENSOR_PIN A0
+
+// ultra sonic sensor
+#define TRIGGER_PIN 3
+#define ECHO_PIN 2
+#define MAX_DISTANCE 250      // cm
+#define SENSOR_ANGLE 45       // deg
+#define MIN_SIDE_DISTANCE 150 // cm
+#define PING_SPEED 250        // ms
+
+#define WARN_DURATION 2000  // ms
+#define BLUETOOTH_SPEED 500 // ms
