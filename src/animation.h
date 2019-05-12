372a84f72043cfb2cb8384d7a281798c5de8d3df
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2019-05-12 12:40:50 +0200

    move LED config to config file

--- src/animation.h
+++ src/animation.h
@@ -3,29 +3,24 @@
 
 #include <FastLED.h>
 
-#define NUM_LEDS 5
-#define LED_PIN 5
-#define BRIGHTNESS 64
-#define LED_TYPE WS2811
-#define COLOR_ORDER GRB
-#define UPDATES_PER_SECOND 200
+#include <config.h>
 
 class Animation
 {
-  public:
-    void setup();
-    void start();
-    void stop();
-    void loop(byte speed);
+public:
+  void setup();
+  void start();
+  void stop();
+  void loop(byte speed);
 
-  private:
-    CRGBPalette16 currentPalette = getEmptyPalette();
-    CRGB leds[NUM_LEDS];
-    bool isRunning = false;
-    uint8_t index = 0;
+private:
+  CRGBPalette16 currentPalette = getEmptyPalette();
+  CRGB leds[NUM_LEDS];
+  bool isRunning = false;
+  uint8_t index = 0;
 
-    CRGBPalette16 getEmptyPalette();
-    CRGBPalette16 getAnimationPalette();
-    void FillLEDsFromPaletteColors(uint8_t colorIndex);
+  CRGBPalette16 getEmptyPalette();
+  CRGBPalette16 getAnimationPalette();
+  void FillLEDsFromPaletteColors(uint8_t colorIndex);
 };
 #endif
--- src/config.h
+++ src/config.h
@@ -8,5 +8,13 @@
 #define MIN_SIDE_DISTANCE 150 // cm
 #define PING_SPEED 250        // ms
 
+// led
+#define NUM_LEDS 5
+#define LED_PIN 5
+#define BRIGHTNESS 64
+#define LED_TYPE WS2811
+#define COLOR_ORDER GRB
+#define UPDATES_PER_SECOND 200
+
 #define WARN_DURATION 2000  // ms
 #define BLUETOOTH_SPEED 500 // ms
